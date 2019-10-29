506139c90b5c46f2274970869b2fda4dc174a65a
nexseedcamille <ykamimori@nexseed.net>
2019-10-29 11:08:56 +0800

    Bootstrap css追加
    ネットワークエラーのため追加

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>Document</title>
   <link rel="stylesheet" href="assets/css/reset.css">
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  <link rel="stylesheet" href="assets/css/bootstrap.min.css">
   <link rel="stylesheet" href="assets/css/style.css">
 </head>
 <body>
@@ -24,15 +24,48 @@
         <section>
             <form class="form-row justify-content-center" action="create.php" method="POST">
                 <div class="col-10 col-md-6 py-2">
-                    <input type="text" class="form-control" placeholder="ADD TODO" name="task">
+                      <input type="text" class="form-control" placeholder="ADD TODO" name="task">
                 </div>
                 <div class="py-2 col-md-3 col-10">
                     <button type="submit" class="col-12 btn btn-primary">ADD</button>
                 </div>
             </form>
         </section>
-        <section>
-
+        <section class="mt-5">
+          <table class="table table-hover">
+              <thead>
+                <tr class="bg-primary text-light">
+                    <th class=>TODO</th>
+                    <th>DUE DATE</th>
+                    <th>STATUS</th>
+                    <th></th>
+                    <th></th>
+                </tr>
+                <tr>
+                    <td>create new website</td>
+                    <td>2019/08/21</td>
+                    <td>NOT YET</td>
+                    <td>
+                        <a class="text-success" href="edit.php">EDIT</a>
+                    </td>
+                    <td>
+                        <a class="text-danger" href="delete.php">DELETE</a>
+                    </td>
+                </tr>
+                <tr>
+                    <td>go to club</td>
+                    <td>2019/10/21</td>
+                    <td>DONE</td>
+                    <td>
+                        <a class="text-success" href="edit.php">EDIT</a>
+                    </td>
+                    <td>
+                        <a class="text-danger" href="delete.php">DELETE</a>
+                    </td>
+                </tr>
+              </thead>
+              <tbody></tbody>
+          </table>
         </section>
     </main>
 
