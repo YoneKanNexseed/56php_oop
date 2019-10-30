d285d0079c1005adab3f93fba4ce4970c6d68cfd
nexseedcamille <ykamimori@nexseed.net>
2019-10-30 12:21:38 +0800

    update編集
    update用のコード追加

---
+++ update.php
@@ -0,0 +1,20 @@
+<?php
+
+// require_once('Models/Todo.php');
+require_once 'Models/Todo.php';
+
+// スーパーグローバル変数を使ってtask , idの値を取得するコードを書いてください
+
+// $task , $id
+// var_dump
+$id = $_POST['id'];
+$task = $_POST['task'];
+
+// var_dump($id);
+// var_dump($task);
+
+$todo = new Todo();
+
+$todo->update($task, $id);
+
+header('Location: index.php');
